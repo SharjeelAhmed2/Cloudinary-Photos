b89f6e51c27e2c9ef28a56366de2ff70ab88025f
SharjeelAhmed2 <sharjeelahmed614@gmail.com>
2024-08-04 21:06:29 +0500

    heart overlay in imageprop.tsx

--- app/gallery/imageprop.tsx
+++ app/gallery/imageprop.tsx
@@ -1,18 +1,19 @@
 "use client";
 
- 
 import { Heart } from "@/components/heart";
 import { CldImage } from "next-cloudinary";
 
-export function CloudinaryImage(props: any){
+export function CloudinaryImage(props: any) {
     return (
-    <div>
-    <CldImage {...props} />
-    <Heart/>
-    </div>
+        <div className="relative w-full h-full">
+            <CldImage {...props} className="object-cover w-full h-full" />
+            <div className="absolute top-2 right-2">
+                <Heart />
+            </div>
+        </div>
     );
-    
 }
+
 
 
 // export function CloudinaryImage({ public_id, ...props}: {public_id:string}){
